484c77c34db38192a344be2f461d52e8920f6661
hunter51218 <gilberth5.12@hotmail.es>
2024-12-02 01:54:11 -0600

    Proyecto

---
+++ src/main/java/com/StreetLand/StreetLand/StreetLandApplication.java
@@ -0,0 +1,13 @@
+package com.StreetLand.StreetLand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreetLandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreetLandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/StreetLand/StreetLand/StreetLandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StreetLand.StreetLand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreetLandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
